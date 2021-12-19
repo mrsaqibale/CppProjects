2fb090802eb513a486d34959bda522ad13ad5d7b
codewithprofessor <92303718+codewithprofessor@users.noreply.github.com>
2021-12-19 08:33:41 +0000

    Update Parking_System_by_Ali.cpp

--- PARKING SYSTEM BY ALI/Parking_System_by_Ali.cpp
+++ PARKING SYSTEM BY ALI/Parking_System_by_Ali.cpp
@@ -1,28 +1,31 @@
 #include<iostream>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    // code by ali
-    
-    
-    cout<<"                                 *************************"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 |    PARKING SYSTEM     |"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 |                       |"<<endl;
-    cout<<"                                 *************************"<<endl;
-    cout<<"                                 *********WELCOME*********"<<endl;
-    cout<<"                                 *******GOOD MORNING******"<<endl;
+    // code by professor 
+    // by 
+    cout<<"                                                   *************************"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   |    PARKING SYSTEM     |"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   |                       |"<<endl;
+    cout<<"                                                   *************************"<<endl;
+    cout<<"                                                   *********WELCOME*********"<<endl;
+    cout<<"                                                   *******GOOD MORNING******"<<endl;
+     cout<<"                                                    *******PROFESSOR******"<<endl;
     
 
     
      int car=0,bike=0,truck=0,blnc=0;
      int bk=0,cr=0,tk=0;
     char ch;
+    do 
+    {
     cout<<"\n\n\n\nSELECT THE ANY OPTION"<<endl;
     cout<<"Enter A for coming Trafic "<<endl;
     cout<<"Enter  B for Outbound Traific "<<endl;
@@ -53,7 +56,7 @@ int main()
             break;    
         
         default:
-        cout<<"Enter the correct Selection\n";
+        cout<<"Please Press the correct Key"<<endl;
             break;
         }
         
@@ -64,7 +67,6 @@ int main()
         cout<<"\n\n         Enter B for Bikes\n         Enter C for Cars\n";
         cout<<"         Enter T for Truks\n";
         cin>>ch;
-        int bk=0,cr=0,tk=0;
 
         switch (ch)
         {
@@ -73,12 +75,13 @@ int main()
         case 'B':
         if(bike!=0)
         {
+            bike==bk;
             bk--;
             break;
         }
         else
         {
-        cout<<"There is no bike in the park";
+        cout<<"There is no bike in the park\n";
         break;
         }    
 
@@ -86,43 +89,61 @@ int main()
         case 'C':
         if(car!=0)
         {
+            
+            car==cr;
             cr--;
             break;
         }
         else
         {
-        cout<<"There is no Car in the park";
+        cout<<"There is no Car in the park\n";
         break;
         }    
         case 't':
         case 'T':
         if(truck!=0)
         {
+            tk=truck;
             tk--;
             break;
         }
         else
         {
-        cout<<"There is no Truck in the park";
+        cout<<"There is no Truck in the park\n";
         break;
         }    
         default:
+        cout<<"Please Press the correct Key"<<endl;
             break;
         }
+        
     }
-    cout<<"                               ***ENTERY REPORT***\n";
+    else 
+    {
+    cout<<"Please Press the correct Key"<<endl;
+    }
+    cout<<"\nCan you want to Run Again(Y/N) : ";
+    cin>>ch;
+    }while(ch=='Y' || ch=='y');  
+    cout<<"\n\n                                 ***ENTERY REPORT***\n\n";
     cout<<"Bikes enter in the park      : "<<bike<<endl;
     cout<<"Cars enter in the park       : "<<car<<endl;
     cout<<"Truck enter in the park      : "<<truck<<endl;
 
      cout<<"\n\n                               ***EXIT  REPORT***\n";
-    cout<<"Bike exits form the park        : "<<bk<<endl;
-    cout<<"Car exits form the park         : "<<cr<<endl;
-    cout<<"Truk exits form the park        : "<<tk<<endl;
+    cout<<"Bike exits form the park        : "<<bk*(-1)<<endl;
+    cout<<"Car exits form the park         : "<<cr*(-1)<<endl;
+    cout<<"Truk exits form the park        : "<<tk*(-1)<<endl;
+
+    cout<<"\n\n                               ***Remaining Trafic***\n";
+     cout<<"Bikes in the Park the park         : "<<bike+bk<<endl;
+     cout<<"Cars in the Park the park          : "<<car+cr<<endl;
+     cout<<"Trucks in the Park the park        : "<<truck+tk<<endl;
 
     cout<<"\n\n                               ***Tex Report***\n";
     cout<<"\nToday Tex        = "<<blnc<<endl;
 
-    
+
+ 
     return 0;
 }
